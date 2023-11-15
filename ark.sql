2f285d39560eccb5346a244db0a726a147b2e89c
Gustavo Alves <gustavo.maciel@sptech.school>
2023-11-15 13:46:44 -0300

    att

--- ark.sql
+++ ark.sql
@@ -10,6 +10,21 @@ CREATE TABLE IF NOT EXISTS especies (
     imagem VARCHAR(255)
 );
 
+CREATE TABLE IF NOT EXISTS contas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    genero VARCHAR(50) NOT NULL,
+    tipoDeJogo VARCHAR(50) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    senha VARCHAR(255) NOT NULL,
+    imagem VARCHAR(255) 
+);
+
+INSERT INTO contas (nome, genero, tipoDeJogo, email, senha) VALUES
+('João', 'Masculino', 'Aventura', 'joao@email.com', 'senha123'),
+('Maria', 'Feminino', 'Estratégia', 'maria@email.com', 'senha456'),
+('Carlos', 'Masculino', 'Esporte', 'carlos@email.com', 'senha789');
+
 INSERT INTO especies (especie, tipoFarm, dieta, imagem) VALUES
 ('argentavis', 'Carne e Derivados', 'Carnívora', 'argentavis.jpg'),
 ('bronto', 'Folhas e Derivados', 'Herbívora', 'bronto.jpg'),
