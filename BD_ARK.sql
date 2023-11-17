b052243bc13aaf6046f1bd31060ba4ed30e4b930
Gustavo Alves <gustavo.maciel@sptech.school>
2023-11-17 00:29:16 -0300

    envio BD

---
+++ BD_ARK.sql
@@ -0,0 +1,35 @@
+CREATE DATABASE ark;
+
+USE ark;
+
+CREATE TABLE especies (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    especie VARCHAR(255) NOT NULL,
+    tipoFarm VARCHAR(255) NOT NULL,
+    dieta VARCHAR(255) NOT NULL,
+    imagem VARCHAR(255)
+);
+
+CREATE TABLE usuario (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    genero VARCHAR(50) NOT NULL,
+    modoJogo VARCHAR(50) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    senha VARCHAR(255) NOT NULL
+);
+
+INSERT INTO usuario (nome, email, modoJogo, senha) VALUES
+('João', 'Masculino', 'PVP', 'joao@email.com', 'senha123'),
+('Maria', 'Feminino', 'PVE', 'maria@email.com', 'senha456'),
+('Carlos', 'Masculino', 'PVE', 'carlos@email.com', 'senha789');
+
+INSERT INTO especies (especie, tipoFarm, dieta, imagem) VALUES
+('argentavis', 'Carne e Derivados', 'Carnívora', 'argentavis.jpg'),
+('bronto', 'Folhas e Derivados', 'Herbívora', 'bronto.jpg'),
+('parassauro', 'Bambu e Derivados', 'Herbívora', 'parassauro.jpg'),
+('rex', 'Carne e Derivados', 'Carnívora', 'rex.jpg');
+
+select * from usuario;
+
+DROP DATABASE ark;
