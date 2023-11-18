37ab354e30f917d079319ba98a88abd13fc26b1c
Gustavo Alves <gustavo.maciel@sptech.school>
2023-11-17 23:21:53 -0300

    att banco

--- site/src/controllers/usuarioController.js
+++ site/src/controllers/usuarioController.js
@@ -1,87 +1,82 @@
 var usuarioModel = require("../models/usuarioModel");
 
 function autenticar(req, res) {
-    var email = req.body.emailServer;
-    var senha = req.body.senhaServer;
+  var email = req.body.emailServer;
+  var senha = req.body.senhaServer;
 
-    if (email == undefined) {
-        res.status(400).send("Seu email está undefined!");
-    } else if (senha == undefined) {
-        res.status(400).send("Sua senha está indefinida!");
-    } else {
+  if (email == undefined) {
+    res.status(400).send("Seu email está undefined!");
+  } else if (senha == undefined) {
+    res.status(400).send("Sua senha está indefinida!");
+  } else {
+    usuarioModel.autenticar(email, senha)
+      .then(
+        function (resultadoAutenticar) {
+          console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
+          console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`);
 
-        usuarioModel.autenticar(email, senha)
-            .then(
-                function (resultadoAutenticar) {
-                    console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
-                    console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`); // transforma JSON em String
-
-                    if (resultadoAutenticar.length == 1) {
-                        console.log(resultadoAutenticar);
-                        res.json({
-                            email: resultadoAutenticar[0].email,
-                            senha: resultadoAutenticar[0].senha
-                        });                       
-                        return true
-                      
-                    } else if (resultadoAutenticar.length == 0) {
-                        res.status(403).send("Email e/ou senha inválido(s)");
-                    } else {
-                        res.status(403).send("Mais de um usuário com o mesmo login e senha!");
-                    }
-                }
-            ).catch(
-                function (erro) {
-                    console.log(erro);
-                    console.log("\nHouve um erro ao realizar o login! Erro: ", erro.sqlMessage);
-                    res.status(500).json(erro.sqlMessage);
-                }
-            );
-    }
-
+          if (resultadoAutenticar.length == 1) {
+            console.log(resultadoAutenticar);
+            res.json({
+              id: resultadoAutenticar[0].id,
+              email: resultadoAutenticar[0].email,
+              senha: resultadoAutenticar[0].senha
+            });
+            return true
+          } else if (resultadoAutenticar.length == 0) {
+            res.status(403).send("Email e/ou senha inválido(s)");
+          } else {
+            res.status(403).send("Mais de um usuário com o mesmo login e senha!");
+          }
+        }
+      )
+      .catch(
+        function (erro) {
+          console.log(erro);
+          console.log("\nHouve um erro ao realizar o login! Erro: ", erro.sqlMessage);
+          res.status(500).json(erro.sqlMessage);
+        }
+      );
+  }
 }
 
 function cadastrar(req, res) {
-    // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
-    var nome = req.body.nomeServer;
-    var email = req.body.emailServer;
-    var senha = req.body.senhaServer;
-    var genero = req.body.generoServer;
-    var jogo = req.body.jogoServer;
+  var nome = req.body.nomeServer;
+  var email = req.body.emailServer;
+  var senha = req.body.senhaServer;
+  var genero = req.body.generoServer;
+  var jogo = req.body.jogoServer;
 
-    // Faça as validações dos valores
-    if (nome == undefined) {
-        res.status(400).send("Seu nome está undefined!");
-    } else if (email == undefined) {
-        res.status(400).send("Seu email está undefined!");
-    } else if (senha == undefined) {
-        res.status(400).send("Sua senha está undefined!");
-    } else if (genero == undefined) {
-        res.status(400).send("Seu genêro está undefined!");
-    } else if (jogo == undefined) {
-        res.status(400).send("Seu modo de jogo está undefined!");
-    } else {
+  console.log("Dados recebidos do frontend:", nome, email, senha, genero, jogo);
 
-        // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha, genero, jogo)
-            .then(
-                function (resultado) {
-                    res.json(resultado);
-                }
-            ).catch(
-                function (erro) {
-                    console.log(erro);
-                    console.log(
-                        "\nHouve um erro ao realizar o cadastro! Erro: ",
-                        erro.sqlMessage
-                    );
-                    res.status(500).json(erro.sqlMessage);
-                }
-            );
-    }
+  if (nome == undefined) {
+    res.status(400).send("Seu nome está undefined!");
+  } else if (email == undefined) {
+    res.status(400).send("Seu email está undefined!");
+  } else if (senha == undefined) {
+    res.status(400).send("Sua senha está undefined!");
+  } else if (genero == undefined) {
+    res.status(400).send("Seu genêro está undefined!");
+  } else if (jogo == undefined) {
+    res.status(400).send("Seu modo de jogo está undefined!");
+  } else {
+    usuarioModel.cadastrar(nome, email, senha, genero, jogo)
+      .then(
+        function (resultado) {
+          res.json(resultado);
+        }
+      )
+      .catch(
+        function (erro) {
+          console.log(erro);
+          console.log("\nHouve um erro ao realizar o cadastro! Erro: ", erro.sqlMessage);
+          res.status(500).json(erro.sqlMessage);
+        }
+      );
+  }
 }
 
 module.exports = {
-    autenticar,
-    cadastrar
+  autenticar,
+  cadastrar
 }
--- site/src/models/usuarioModel.js
+++ site/src/models/usuarioModel.js
@@ -1,28 +1,32 @@
-var database = require("../database/config")
+var database = require("../database/config");
 
 function autenticar(email, senha) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
-    var instrucao = `
-        SELECT email, senha FROM usuario WHERE email = '${email}' AND senha = '${senha}';
-    `;
-    console.log("Executando a instrução SQL: \n" + instrucao);
-    return database.executar(instrucao);
+  console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha);
+  var instrucao = `
+      SELECT id, email, senha FROM usuario WHERE email = '${email}' AND senha = '${senha}';
+  `;
+  console.log("Executando a instrução SQL: \n" + instrucao);
+  return database.executar(instrucao);
 }
 
-// Coloque os mesmos parâmetros aqui. Vá para a var instrucao
-function cadastrar(nome, email, jogo, genero, senha) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, jogo, genero, senha);
-    
-    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
-    //  e na ordem de inserção dos dados.
-    var instrucao = `
-        INSERT INTO usuario (nome, email, senha, modoJogo, genero) VALUES ('${nome}', '${email}', '${jogo}', '${genero}', '${senha}');
-        `;
-    console.log("Executando a instrução SQL: \n" + instrucao);
-    return database.executar(instrucao);
+function cadastrar(nome, email, senha, genero, jogo) {
+  console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, jogo, genero, senha);
+  var instrucao = `
+      INSERT INTO usuario (nome, email, senha, modoJogo, genero) VALUES ('${nome}', '${email}', '${senha}', '${jogo}', '${genero}');
+  `;
+  console.log("Executando a instrução SQL: \n" + instrucao);
+  return database.executar(instrucao)
+    .then(result => {
+      console.log("Operação bem-sucedida:", result);
+      return result; // Pode retornar alguma informação relevante para sua aplicação
+    })
+    .catch(error => {
+      console.error("Erro na operação:", error);
+      throw error; // Propagar o erro para quem chamou esta função
+    });
 }
 
 module.exports = {
-    autenticar,
-    cadastrar
+  autenticar,
+  cadastrar
 };
